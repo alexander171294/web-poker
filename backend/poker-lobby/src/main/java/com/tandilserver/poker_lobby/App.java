63316f9708bf1f9a8d2d6bfa7aeac582c0695c12
Alexander <alexander.eberle@aizensoft.com>
2018-06-24 01:33:14 -0300

    Agregamos soporte para repositorios en ese package en particular.

--- backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/App.java
+++ backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/App.java
@@ -3,9 +3,11 @@ package com.tandilserver.poker_lobby;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.tandilserver.poker_lobby.dataBase.domain")
+@EnableJpaRepositories("ar.com.sdc.test.dataBase.repository")
 public class App 
 {
     public static void main( String[] args )

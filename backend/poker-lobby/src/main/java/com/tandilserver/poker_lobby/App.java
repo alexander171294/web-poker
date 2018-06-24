17ca563ca6ac7c0c1a8f2514ed9a22596cc55596
Alexander <alexander.eberle@aizensoft.com>
2018-06-24 01:32:40 -0300

    Agregamos tabla de pruebas para mysql database. y config spring.

--- backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/App.java
+++ backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/App.java
@@ -2,8 +2,10 @@ package com.tandilserver.poker_lobby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.tandilserver.poker_lobby.dataBase.domain")
 public class App 
 {
     public static void main( String[] args )
---
+++ backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/dataBase/repository/TablatestRepository.java
@@ -0,0 +1,10 @@
+package com.tandilserver.poker_lobby.dataBase.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tandilserver.poker_lobby.dataBase.domain.Tablatest;
+
+public interface TablatestRepository extends CrudRepository<Tablatest, Integer> {
+//	Employee findByFirstName(String firstName);
+//	List<Employee> findByLastName(String lastName);
+}

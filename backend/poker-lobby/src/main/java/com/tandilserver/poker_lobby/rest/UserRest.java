2de395670a8f6753d02917ac0e38c22d7ed9af97
Alexander <alexander.eberle@aizensoft.com>
2018-06-27 21:21:34 -0300

    Update package

--- backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/rest/UserRest.java
+++ backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/rest/UserRest.java
@@ -1,5 +1,16 @@
 package com.tandilserver.poker_lobby.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userRest")
 public class UserRest {
 
+	@RequestMapping(path="/helloWorld", method=RequestMethod.GET)
+	public String tablatest() {
+		return "Hola Mundo";
+	}
+	
 }

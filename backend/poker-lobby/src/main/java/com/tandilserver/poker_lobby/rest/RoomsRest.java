cac1b5e9b3004001b4b7d66268253d5c1cc6c4f0
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-01-21 15:30:12 -0300

    encapsulating rest response.

--- backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/rest/RoomsRest.java
+++ backend/poker-lobby/src/main/java/com/tandilserver/poker_lobby/rest/RoomsRest.java
@@ -1,5 +1,6 @@
 package com.tandilserver.poker_lobby.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tandilserver.poker_lobby.dataBase.customTypes.ServerTypes;
 import com.tandilserver.poker_lobby.dataBase.domain.Rooms;
 import com.tandilserver.poker_lobby.dataBase.repository.RoomsRepository;
+import com.tandilserver.poker_lobby.rest.dto.RoomsOut;
 
 @RestController
 @RequestMapping("/roomsRest")
@@ -18,9 +20,25 @@ public class RoomsRest {
 	@Autowired
 	private RoomsRepository roomsRepo;
 
-	@RequestMapping(path="/sitngo", method=RequestMethod.POST)
-	public List<Rooms> getRoomsOfSitNGo() {
-		return roomsRepo.getRoomsOfType(ServerTypes.SIT_N_GO);
+	@RequestMapping(path="/sitngo", method=RequestMethod.GET)
+	public List<RoomsOut> getRoomsOfSitNGo() {
+		List<Rooms> rooms = roomsRepo.getRoomsOfType(ServerTypes.SIT_N_GO);
+		List<RoomsOut> out = new ArrayList<RoomsOut>();
+		for(Rooms room: rooms) {
+			RoomsOut item = new RoomsOut();
+			item.setBlind(room.getBlind());
+			item.setId_server(room.getId_server());
+			item.setIp(room.getIp());
+			item.setLimit_bet(room.getLimit_bet());
+			item.setMin_bet(room.getMin_bet());
+			item.setOfficialServer(room.isOfficialServer());
+			item.setPlayers(room.getPlayers());
+			item.setPort(room.getPort());
+			item.setServer_name(room.getServer_name());
+			item.setServer_type(room.getServer_type());
+			out.add(item);
+		}
+		return out;
 	}
 	
 	@RequestMapping(path="/tournament", method=RequestMethod.POST)

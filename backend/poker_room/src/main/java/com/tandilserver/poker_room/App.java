e54aa16011c7d2cd62f9be19f5b3517685c1a4ef
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-01-21 11:21:49 -0300

    Springboot annotations.-

--- backend/poker_room/src/main/java/com/tandilserver/poker_room/App.java
+++ backend/poker_room/src/main/java/com/tandilserver/poker_room/App.java
@@ -1,7 +1,11 @@
 package com.tandilserver.poker_room;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
+@Configuration
 public class App 
 {
 	

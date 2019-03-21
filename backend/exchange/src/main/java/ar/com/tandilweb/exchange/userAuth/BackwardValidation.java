281c2908401b334d004c119e846da35515c8c692
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:36:01 -0300

    Finishing BackwardValidation Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/userAuth/BackwardValidation.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/userAuth/BackwardValidation.java
@@ -1,8 +1,12 @@
 package ar.com.tandilweb.exchange.userAuth;
 
 import ar.com.tandilweb.exchange.UserAuthSchema;
+import ar.com.tandilweb.exchange.userAuth.types.ChallengeActions;
 
 public class BackwardValidation extends UserAuthSchema {
+	
+	public long idChallenge;
+	public ChallengeActions action;
 	
 	public BackwardValidation() {
 		super("backwardValidation");

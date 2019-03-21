40b84cba6bdf0033963a48be8893220ce98d0579
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:35:50 -0300

    Finishing Challenge Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/userAuth/Challenge.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/userAuth/Challenge.java
@@ -1,8 +1,13 @@
 package ar.com.tandilweb.exchange.userAuth;
 
 import ar.com.tandilweb.exchange.UserAuthSchema;
+import ar.com.tandilweb.exchange.userAuth.types.ChallengeActions;
 
 public class Challenge extends UserAuthSchema {
+	
+	public String claimToken;
+	public long roomID;
+	public ChallengeActions action;
 	
 	public Challenge() {
 		super("challenge");

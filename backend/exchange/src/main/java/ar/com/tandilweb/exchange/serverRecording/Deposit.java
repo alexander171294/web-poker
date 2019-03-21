0fa012bff3cb6b70a1d951e7d63cbf232583617d
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:32:31 -0300

    Finishing Deposit Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/serverRecording/Deposit.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/serverRecording/Deposit.java
@@ -4,6 +4,11 @@ import ar.com.tandilweb.exchange.ServerRecordingSchema;
 
 public class Deposit extends ServerRecordingSchema {
 	
+	public long userID;
+	public long challengeID;
+	public String claimToken;
+	public long coins;
+	
 	public Deposit() {
 		super("deposit");
 	}

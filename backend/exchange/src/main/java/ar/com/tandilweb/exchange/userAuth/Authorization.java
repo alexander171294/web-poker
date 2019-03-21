5c8fb4bd5c25c81ec1c7c11bc80782ded42ceeb0
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:35:17 -0300

    Finishing Authorization Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/userAuth/Authorization.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/userAuth/Authorization.java
@@ -3,6 +3,8 @@ package ar.com.tandilweb.exchange.userAuth;
 import ar.com.tandilweb.exchange.UserAuthSchema;
 
 public class Authorization extends UserAuthSchema {
+	
+	public long userID;
 
 	public Authorization() {
 		super("authorization");

608ab47037dcc69d38f74f13b833bbdd65041c57
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 11:20:20 -0300

    Generic schema

---
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/Schema.java
@@ -0,0 +1,10 @@
+package ar.com.tandilweb.exchange;
+
+public class Schema {
+	
+	public final String protocol = "eppr/sn";
+	public final String version = "1.0.0";
+	public String namespace;
+	public String schema;
+
+}

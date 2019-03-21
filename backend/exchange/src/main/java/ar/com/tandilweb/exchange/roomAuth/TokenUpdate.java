dfb21fe283e595af3b978693f6645eab61824e72
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:29:22 -0300

    Finish TokenUpdate Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomAuth/TokenUpdate.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomAuth/TokenUpdate.java
@@ -4,6 +4,8 @@ import ar.com.tandilweb.exchange.RoomAuthSchema;
 
 public class TokenUpdate extends RoomAuthSchema {
 	
+	public String securityToken;
+	
 	public TokenUpdate() {
 		super("tokenUpdate");
 	}

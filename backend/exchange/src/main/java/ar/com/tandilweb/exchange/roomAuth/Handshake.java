f4b3c5dc670acc744caf9a698844af9971d22e24
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:27:12 -0300

    Finish Handshake Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomAuth/Handshake.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomAuth/Handshake.java
@@ -4,6 +4,15 @@ import ar.com.tandilweb.exchange.RoomAuthSchema;
 
 public class Handshake extends RoomAuthSchema {
 	
+	public String name;
+	public long serverID;
+	public String accessPassword;
+	public String securityToken;
+	public String gproto;
+	public int maxPlayers;
+	public String description;
+	public int minCoinsForAccess;
+	
 	public Handshake() {
 		super("handshake");
 	}

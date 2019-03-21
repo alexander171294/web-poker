66a15417ea79ff3bd0e95094ae1b95dfff17eb2d
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:56:09 -0300

    Finishing UserDetail Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/UserDetail.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/UserDetail.java
@@ -2,7 +2,10 @@ package ar.com.tandilweb.exchange.orchestratorLSL;
 
 import ar.com.tandilweb.exchange.OrchestratorLSLSchema;
 
-public class UserDetail extends OrchestratorLSLSchema {
+public class UserDetail<UserDataTemplate> extends OrchestratorLSLSchema {
+	
+	public long userID;
+	public UserDataTemplate data;
 	
 	public UserDetail() {
 		super("userDetail");

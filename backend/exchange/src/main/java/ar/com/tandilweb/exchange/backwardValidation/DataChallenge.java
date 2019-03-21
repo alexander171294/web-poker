ae3e1fd7d6695874a8156eea5c8b44412fa0c0be
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:38:43 -0300

    Finishing DataChallenge Schema.

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/backwardValidation/DataChallenge.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/backwardValidation/DataChallenge.java
@@ -2,7 +2,11 @@ package ar.com.tandilweb.exchange.backwardValidation;
 
 import ar.com.tandilweb.exchange.BackwardValidationSchema;
 
-public class DataChallenge extends BackwardValidationSchema {
+public class DataChallenge<UserDataTemplate> extends BackwardValidationSchema {
+	
+	public long idUser;
+	public String claimToken;
+	public UserDataTemplate userData;
 	
 	public DataChallenge() {
 		super("dataChallenge");

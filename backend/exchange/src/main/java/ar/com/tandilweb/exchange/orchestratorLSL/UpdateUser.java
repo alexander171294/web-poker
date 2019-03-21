c58b3c6e267e92c0fd2d37d97bd17410e12667b5
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:56:01 -0300

    Finishing UpdateUser Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/UpdateUser.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/UpdateUser.java
@@ -2,7 +2,10 @@ package ar.com.tandilweb.exchange.orchestratorLSL;
 
 import ar.com.tandilweb.exchange.OrchestratorLSLSchema;
 
-public class UpdateUser extends OrchestratorLSLSchema {
+public class UpdateUser<UserDataTemplate> extends OrchestratorLSLSchema {
+	
+	public long userID;
+	public UserDataTemplate data;
 	
 	public UpdateUser() {
 		super("updateUser");

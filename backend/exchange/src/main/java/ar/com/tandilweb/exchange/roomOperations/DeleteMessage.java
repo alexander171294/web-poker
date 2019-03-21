251ba5fd4615aa6d44872033fbc1a7a13d58d2bf
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:46:23 -0300

    Finishing DeleteMessage Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomOperations/DeleteMessage.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomOperations/DeleteMessage.java
@@ -4,6 +4,8 @@ import ar.com.tandilweb.exchange.RoomOperationsSchema;
 
 public class DeleteMessage extends RoomOperationsSchema {
 	
+	public long messageID;
+	
 	public DeleteMessage() {
 		super("deleteMessage");
 	}

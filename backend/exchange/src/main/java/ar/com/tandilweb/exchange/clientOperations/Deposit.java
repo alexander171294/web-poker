5a97efc72f8d30b196545dc8f3034fd0faf572ae
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:57:40 -0300

    Finishing Deposit Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/clientOperations/Deposit.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/clientOperations/Deposit.java
@@ -4,6 +4,8 @@ import ar.com.tandilweb.exchange.ClientOperationsSchema;
 
 public class Deposit extends ClientOperationsSchema {
 	
+	public long coins;
+	
 	public Deposit() {
 		super("deposit");
 	}

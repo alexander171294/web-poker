ce24a428428215632d544f6d56ab2f8f36549b01
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:55:28 -0300

    Finishing FriendshipRequest Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/FriendshipRequest.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/FriendshipRequest.java
@@ -4,6 +4,8 @@ import ar.com.tandilweb.exchange.OrchestratorLSLSchema;
 
 public class FriendshipRequest extends OrchestratorLSLSchema {
 	
+	public long idTarget;
+	
 	public FriendshipRequest() {
 		super("friendshipRequest");
 	}

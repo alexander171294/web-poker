110e3a76c4f75620e7e3e90ac71e791831bb72d9
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:29:13 -0300

    Finish SignupResponse Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomAuth/SignupResponse.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomAuth/SignupResponse.java
@@ -4,6 +4,9 @@ import ar.com.tandilweb.exchange.RoomAuthSchema;
 
 public class SignupResponse extends RoomAuthSchema {
 	
+	public long serverID;
+	public String securityToken;
+	
 	public SignupResponse() {
 		super("signupResponse");
 	}

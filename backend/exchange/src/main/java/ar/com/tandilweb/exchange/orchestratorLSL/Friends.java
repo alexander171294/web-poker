b9d79ae59808319e0729c6142f9ee7413d2ad0af
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:55:15 -0300

    Finished FriendshipRequest Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/Friends.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/Friends.java
@@ -1,8 +1,12 @@
 package ar.com.tandilweb.exchange.orchestratorLSL;
 
+import java.util.List;
+
 import ar.com.tandilweb.exchange.OrchestratorLSLSchema;
 
-public class Friends extends OrchestratorLSLSchema {
+public class Friends<UserDataTemplate> extends OrchestratorLSLSchema {
+	
+	public List<UserDataTemplate> list;
 	
 	public Friends() {
 		super("friends");

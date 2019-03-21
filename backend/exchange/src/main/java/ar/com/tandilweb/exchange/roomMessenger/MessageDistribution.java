efe9e4863a306cdbb6bdcde13a1e9daf2a6720b3
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:41:37 -0300

    Finishing MessageDistribution Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomMessenger/MessageDistribution.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/roomMessenger/MessageDistribution.java
@@ -1,8 +1,16 @@
 package ar.com.tandilweb.exchange.roomMessenger;
 
+import java.util.Date;
+
 import ar.com.tandilweb.exchange.RoomMessengerSchema;
 
 public class MessageDistribution extends RoomMessengerSchema {
+	
+	public long messageID;
+	public String message;
+	public long userID;
+	public String userName;
+	public Date date;
 	
 	public MessageDistribution() {
 		super("messageDistribution");

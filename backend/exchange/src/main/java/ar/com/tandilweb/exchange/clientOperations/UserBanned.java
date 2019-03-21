aba0d50bce78edac46d58c69a79a7b52f2ea6fa5
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:57:57 -0300

    Finishing UserBanned Schema.

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/clientOperations/UserBanned.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/clientOperations/UserBanned.java
@@ -4,6 +4,8 @@ import ar.com.tandilweb.exchange.ClientOperationsSchema;
 
 public class UserBanned extends ClientOperationsSchema {
 	
+	public long userID;
+	
 	public UserBanned() {
 		super("userBanned");
 	}

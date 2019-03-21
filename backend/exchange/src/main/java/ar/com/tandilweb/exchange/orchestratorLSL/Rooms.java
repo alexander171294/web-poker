aa1d1d6081c6dab4e3c1a3c12926cc6b5b066ddd
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:55:54 -0300

    Finishing Rooms Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/Rooms.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/orchestratorLSL/Rooms.java
@@ -1,8 +1,12 @@
 package ar.com.tandilweb.exchange.orchestratorLSL;
 
+import java.util.List;
+
 import ar.com.tandilweb.exchange.OrchestratorLSLSchema;
 
-public class Rooms extends OrchestratorLSLSchema {
+public class Rooms<RoomDataTemplate> extends OrchestratorLSLSchema {
+	
+	public List<RoomDataTemplate> list;
 	
 	public Rooms() {
 		super("rooms");

cbe60fbbb6f215dca0aafcdc13f930b99d7939a4
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-21 12:32:13 -0300

    Finish SuccessDeposit Schema

--- backend/exchange/src/main/java/ar/com/tandilweb/exchange/serverRecording/SuccessDeposit.java
+++ backend/exchange/src/main/java/ar/com/tandilweb/exchange/serverRecording/SuccessDeposit.java
@@ -4,6 +4,8 @@ import ar.com.tandilweb.exchange.ServerRecordingSchema;
 
 public class SuccessDeposit extends ServerRecordingSchema {
 	
+	public long userID;
+	
 	public SuccessDeposit() {
 		super("successDeposit");
 	}

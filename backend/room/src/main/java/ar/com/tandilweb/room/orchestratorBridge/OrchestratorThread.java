3dedbb7a8c6bafed4912f6a904c3dfd3629f2ecf
alexander.eberle <alexander.eberle@sdc19.sdc.ad>
2019-03-22 19:13:01 -0300

    Orchestrator client cleanned.

--- backend/room/src/main/java/ar/com/tandilweb/room/orchestratorBridge/OrchestratorThread.java
+++ backend/room/src/main/java/ar/com/tandilweb/room/orchestratorBridge/OrchestratorThread.java
@@ -1,5 +1,109 @@
 package ar.com.tandilweb.room.orchestratorBridge;
 
-public class OrchestratorThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySources(value = {@PropertySource("classpath:room.properties")})
+public class OrchestratorThread implements Runnable, ApplicationListener<ContextRefreshedEvent> {
+	
+	public static Logger logger = LoggerFactory.getLogger(OrchestratorThread.class);
+	private Thread thread;
+	
+	@Value("${room.register.listenPort}")
+    private volatile int remoteListenerPort;
+	
+//	@Autowired
+//	private TaskScheduler taskScheduler;
+	
+	BufferedReader socketBufferReader;
+	private PrintWriter socketBufferOutput;
+	
+//	@Autowired
+//	private ApplicationContext context;
+	
+	private Socket socket;
+	private boolean scanning;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		// TODO Auto-generated method stub
+		if(this.thread == null) {
+			this.thread = new Thread(this);
+			this.thread.start();
+			logger.debug("RegisterService Thread created.");
+		}
+	}
+	
+	@Override
+	public void run() {
+        try {
+            createSocketConneciton();
+        } catch (IOException e) {
+            logger.error("Connection IO Exception, ", e);
+        }
+        try{
+        	main();
+        } catch(Exception e){
+            logger.error("Socket read Error", e);
+        } finally{
+            try {
+				socketBufferOutput.close();
+				socketBufferReader.close();
+	            socket.close();
+	            logger.error("Connection Closed");
+			} catch (IOException e) {
+				logger.error("Loop close error", e);
+			}
+        }
+	}
+	
+	private void main() throws IOException {
+		do {
+			String message = socketBufferReader.readLine();
+			processIncommingMessage(message);
+		} while(scanning);
+	}
+	
+	private void createSocketConneciton() throws UnknownHostException, IOException {
+		socket = new Socket("127.0.0.1", this.remoteListenerPort);
+		socketBufferReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        socketBufferOutput = new PrintWriter(socket.getOutputStream());
+	}
+	
+	public void sendDataToServer(String data) {
+		socketBufferOutput.println(data);
+		socketBufferOutput.flush();
+	}
+	
+	private String getMyLocalIP() {
+		try {
+			return InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException ukHostException) {
+			logger.error("Unknown Host Exception", ukHostException);
+			return null;
+		}
+	}
+	
+	private void processIncommingMessage(String message) {
+		// TODO: pending...
+	}
 
 }

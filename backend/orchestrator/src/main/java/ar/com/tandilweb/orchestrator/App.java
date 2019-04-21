3c59bd15d63fe7458f0bd1a0b42831cd2654dcaa
Alexander Eberle <alexander171294@gmail.com>
2019-04-21 18:51:08 -0300

    Initial jdbc configuration.

--- backend/orchestrator/src/main/java/ar/com/tandilweb/orchestrator/App.java
+++ backend/orchestrator/src/main/java/ar/com/tandilweb/orchestrator/App.java
@@ -3,13 +3,18 @@ package ar.com.tandilweb.orchestrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
+import ar.com.tandilweb.orchestrator.persistence.JDBConfig;
+
 @SpringBootApplication
 @PropertySources(value = {
-			@PropertySource("classpath:serverRegisterConfig.properties")
+			@PropertySource("classpath:serverRegisterConfig.properties"),
+			@PropertySource("classpath:database.properties"),
 		})
+@Import({ JDBConfig.class })
 public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )

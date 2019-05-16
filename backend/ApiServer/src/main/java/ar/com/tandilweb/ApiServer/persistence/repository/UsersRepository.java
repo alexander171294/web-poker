8e3e862677258aa89657e2b87f7ea0fa225ebb8e
Alexander Eberle <alexander.eberle@tandilweb.com.ar>
2019-05-15 23:56:38 -0300

    Added queries to UserRepository.

--- backend/ApiServer/src/main/java/ar/com/tandilweb/ApiServer/persistence/repository/UsersRepository.java
+++ backend/ApiServer/src/main/java/ar/com/tandilweb/ApiServer/persistence/repository/UsersRepository.java
@@ -1,5 +1,18 @@
 package ar.com.tandilweb.ApiServer.persistence.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import ar.com.tandilweb.ApiServer.persistence.BaseRepository;
@@ -7,30 +20,97 @@ import ar.com.tandilweb.ApiServer.persistence.domain.Users;
 
 @Repository
 public class UsersRepository extends BaseRepository<Users, Long> {
+	
+	public static Logger logger = LoggerFactory.getLogger(UsersRepository.class);
 
 	@Override
 	public Users create(Users record) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			final String sql = "INSERT INTO users "
+					+ "(nick_name, email, password, chips, photo, badLogins) "
+					+ "VALUES(?,?,?,?,?,?)";
+			KeyHolder holder = new GeneratedKeyHolder();
+			jdbcTemplate.update(new PreparedStatementCreator() {
+				public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+					PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+					ps.setString(1, record.getNick_name());
+					ps.setString(2, record.getEmail());
+					ps.setString(3, record.getPassword());
+					ps.setLong(4, record.getChips());
+					ps.setString(5, record.getPhoto());
+					ps.setInt(6, record.getBadLogins());
+					return ps;
+				}
+			}, holder);
+			record.setId_user(holder.getKey().longValue());
+			return record;
+		} catch(DataAccessException e) {
+			logger.error("UsersRepository::create", e);
+			return null;
+		}
 	}
 
 	@Override
 	public void update(Users record) {
-		// TODO Auto-generated method stub
-		
+		try {
+			final String sql = "UPDATE users SET email = ?, password = ?, chips = ?, photo = ?, badLogins = ? WHERE id_user = ?";
+			jdbcTemplate.update(sql, new Object[] {
+					record.getEmail(),
+					record.getPassword(),
+					record.getChips(),
+					record.getPhoto(),
+					record.getBadLogins(),
+					record.getId_user()
+			});
+		} catch(DataAccessException e) {
+			logger.error("UsersRepository::update", e);
+		}
 	}
 
 	@Override
 	public Users findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return jdbcTemplate.queryForObject(
+                "SELECT * FROM users WHERE id_user = ?",
+                new Object[]{id}, new UsersRowMapper());
+		} catch(DataAccessException e) {
+			return null;
+		}
 	}
 	
 	public boolean checkEmailUser(String email, String nick_name) {
-		return false;
+		try {
+			int total = jdbcTemplate.queryForObject(
+                "SELECT count(id_user) FROM users WHERE email = ? OR nick_name = ?",
+                new Object[]{email, nick_name}, Integer.class);
+			return total > 0;
+		} catch(DataAccessException e) {
+			return false;
+		}
 	}
 	
 	public Users findByEmailOrUser(String umail) {
-		return null;
+		try {
+			return jdbcTemplate.queryForObject(
+                "SELECT * FROM users WHERE email = ? OR nick_name = ?",
+                new Object[]{umail, umail}, new UsersRowMapper());
+		} catch(DataAccessException e) {
+			return null;
+		}
 	}
+	
+	class UsersRowMapper implements RowMapper<Users> {
+		public Users mapRow(ResultSet rs, int rowNum) throws SQLException {
+	        return new Users(
+	        		rs.getLong("id_user"),
+	        		rs.getString("nick_name"),
+	        		rs.getString("email"),
+	        		rs.getString("password"),
+	        		rs.getLong("chips"),
+	        		rs.getString("photo"),
+	        		rs.getShort("badLogins")
+	        		);
+	    }
+	}
+	
 }

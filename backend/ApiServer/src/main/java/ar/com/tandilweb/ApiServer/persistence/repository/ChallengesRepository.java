356ee6992da3845258031b915c5f3ce25fa7b22f
Alexander Eberle <alexander.eberle@tandilweb.com.ar>
2019-05-16 00:12:15 -0300

    Queries added to ChallengesRepository

--- backend/ApiServer/src/main/java/ar/com/tandilweb/ApiServer/persistence/repository/ChallengesRepository.java
+++ backend/ApiServer/src/main/java/ar/com/tandilweb/ApiServer/persistence/repository/ChallengesRepository.java
@@ -1,29 +1,81 @@
 package ar.com.tandilweb.ApiServer.persistence.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import ar.com.tandilweb.ApiServer.persistence.BaseRepository;
 import ar.com.tandilweb.ApiServer.persistence.domain.Challenges;
+import ar.com.tandilweb.ApiServer.persistence.domain.Sessions;
+import ar.com.tandilweb.ApiServer.persistence.repository.SessionsRepository.SessionsRowMapper;
 
 @Repository
 public class ChallengesRepository extends BaseRepository<Challenges, Long> {
+	
+	public static Logger logger = LoggerFactory.getLogger(ChallengesRepository.class);
 
 	@Override
 	public Challenges create(Challenges record) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			final String sql = "INSERT INTO challenges "
+					+ "(id_user, id_room, challenge) "
+					+ "VALUES(?,?,?)";
+			KeyHolder holder = new GeneratedKeyHolder();
+			jdbcTemplate.update(new PreparedStatementCreator() {
+				public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+					PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+					ps.setLong(1, record.getId_user());
+					ps.setLong(2, record.getId_room());
+					ps.setString(3, record.getChallenge());
+					return ps;
+				}
+			}, holder);
+			record.setChallengeID(holder.getKey().longValue());
+			return record;
+		} catch(DataAccessException e) {
+			logger.error("ChallengesRepository::create", e);
+			return null;
+		}
 	}
 
 	@Override
+	@Deprecated
 	public void update(Challenges record) {
-		// TODO Auto-generated method stub
-		
+		// you can't update challenges.
+		logger.warn("Deprecated update function called. (Code Ignored)");
 	}
 
 	@Override
 	public Challenges findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return jdbcTemplate.queryForObject(
+                "SELECT * FROM sessions WHERE id_session = ?",
+                new Object[]{id}, new ChallengesRowMapper());
+		} catch(DataAccessException e) {
+			return null;
+		}
+	}
+	
+	class ChallengesRowMapper implements RowMapper<Challenges> {
+		public Challenges mapRow(ResultSet rs, int rowNum) throws SQLException {
+	        return new Challenges(
+	        		rs.getLong("challengeID"),
+	        		rs.getLong("id_user"),
+	        		rs.getLong("id_room"),
+	        		rs.getString("challenge") // claimToken
+	        		);
+	    }
 	}
 	
 }

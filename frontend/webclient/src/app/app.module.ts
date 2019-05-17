42c68cd2af672190cac823657f75e1308e6b2210
Alexander Eberle <alexander.eberle@tandilweb.com.ar>
2019-05-16 23:44:55 -0300

    web design

--- frontend/webclient/src/app/app.module.ts
+++ frontend/webclient/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { LobbyComponent } from './lobby/lobby.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

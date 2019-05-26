71fc17f6473a4eb3870a2ac0548794044579c2f4
Alexander Eberle <alexander171294@gmail.com>
2019-05-26 06:48:24 -0300

    binding profile data with backend.

--- frontend/webclient/src/app/lobby/profile-section/profile-section.component.ts
+++ frontend/webclient/src/app/lobby/profile-section/profile-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SessionService } from 'src/app/providers/session.service';
 import { UsersService } from 'src/app/providers/users/users.service';
+import { UserProfile } from 'src/app/providers/users/userProfile';
 
 @Component({
   selector: 'app-profile-section',
@@ -9,10 +10,22 @@ import { UsersService } from 'src/app/providers/users/users.service';
 })
 export class ProfileSectionComponent implements OnInit {
 
+  private userData: UserProfile;
+
   constructor(private session: SessionService, private userSrv: UsersService) { }
 
   ngOnInit() {
-    
+    this.userSrv.getProfile(this.session.getSessionInfo().userID).subscribe(
+      data => this.onInfoUpdated(data),
+      err => {
+        // TODO: improve this alert:
+        alert('Connection error.');
+      }
+    );
+  }
+
+  onInfoUpdated(data: UserProfile) {
+    this.userData = data;
   }
 
 }

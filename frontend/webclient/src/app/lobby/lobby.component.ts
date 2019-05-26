e6b5536ffbfbbd005f595d56c81633a6f0dedeaf
Alexander Eberle <alexander171294@gmail.com>
2019-05-26 06:48:09 -0300

    redirect to login

--- frontend/webclient/src/app/lobby/lobby.component.ts
+++ frontend/webclient/src/app/lobby/lobby.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../providers/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lobby',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LobbyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private session: SessionService, private router: Router) { }
 
   ngOnInit() {
+    if(!this.session.isSessionActive()) {
+      this.router.navigate(['/login']);
+    }
   }
 
 }

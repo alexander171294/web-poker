ede6443670086307fa3cbf41df798842fd7b4f64
alexander171294 <alexander171294@gmail.com>
2019-06-08 13:49:48 -0300

    chat box design.

--- frontend/webclient/src/app/room/chat-box/chat-box.component.ts
+++ frontend/webclient/src/app/room/chat-box/chat-box.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatBoxComponent implements OnInit {
 
+  opened: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.opened = false;
   }
 
 }

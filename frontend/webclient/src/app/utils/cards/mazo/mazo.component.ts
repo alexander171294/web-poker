c49e98fc31b03e60abffdf5095028831fbc834de
alexander171294 <alexander171294@gmail.com>
2019-06-15 22:24:38 -0300

    Full room design ready

--- frontend/webclient/src/app/utils/cards/mazo/mazo.component.ts
+++ frontend/webclient/src/app/utils/cards/mazo/mazo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-mazo',
+  selector: 'app-deck',
   templateUrl: './mazo.component.html',
   styleUrls: ['./mazo.component.scss']
 })

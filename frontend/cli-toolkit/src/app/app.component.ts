b1faa7233bc9e81d828bb3280595e27d4a04aafc
Alexander Eberle <alexander171294@gmail.com>
2019-10-14 14:14:47 -0300

    commands.

--- frontend/cli-toolkit/src/app/app.component.ts
+++ frontend/cli-toolkit/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren } from '@angular/core';
 import { RoomService } from './services/room.service';
 import { TerminalService } from './services/terminal.service';
 
@@ -13,6 +13,8 @@ export class AppComponent {
   private roomServer = '127.0.0.1:8081';
   private orchestratorServer = '127.0.0.1:8082';
   private apiServer = '127.0.0.1:8083';
+  private commandPrompt: string;
+
 
   private terminalMessages = [
     'Ready'
@@ -26,5 +28,32 @@ export class AppComponent {
 
   connectRoomServer() {
     this.room.connect(this.roomServer);
+  }
+
+  keyUp(evt) {
+    if(evt.keyCode === 13){
+      // eval command.
+    }
+  }
+
+  // Command writter helper // 
+  ingressCMD() {
+    this.commandPrompt = 'room.ingress user:"NickName" photo:"PhotoURL" chips:"4500"';
+    document.getElementById('commandPrompt').focus();
+  }
+
+  depositCMD() {
+    this.commandPrompt = 'room.deposit user:"57" coins:"500" challengeID:"16" claimToken:"xMjM0NTY3ODkwIiwibmFtZSI6Ikpva"';
+    document.getElementById('commandPrompt').focus();
+  }
+
+  roomChallengeCMD() {
+    this.commandPrompt = 'apisrv.challenge challengeID:"16" claimToken:"xMjM0NTY3ODkwIiwibmFtZSI6Ikpva"';
+    document.getElementById('commandPrompt').focus();
+  }
+
+  backwardValidationCMD() {
+    this.commandPrompt = 'room.backwardValidation challengeID:"16"';
+    document.getElementById('commandPrompt').focus();
   }
 }

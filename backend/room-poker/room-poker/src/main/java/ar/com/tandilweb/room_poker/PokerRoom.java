aec67479c2b29f95f0e7600c4bd7d58b7debb8ea
Alexander Eberle <2080955+alexander171294@users.noreply.github.com>
2019-10-23 15:19:24 -0300

    Initial poker room class.

---
+++ backend/room-poker/room-poker/src/main/java/ar/com/tandilweb/room_poker/PokerRoom.java
@@ -0,0 +1,30 @@
+package ar.com.tandilweb.room_poker;
+
+import ar.com.tandilweb.exchange.gameProtocol.SchemaGameProto;
+import ar.com.tandilweb.room_int.GameCtrlInt;
+import ar.com.tandilweb.room_int.handlers.SessionHandlerInt;
+import ar.com.tandilweb.room_int.handlers.dto.UserData;
+
+public class PokerRoom implements GameCtrlInt {
+
+	public void setUsersInTableRef(UserData[] usersInTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void checkStartGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void dumpSnapshot() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void receivedMessage(SchemaGameProto message, String socketSessionID, SessionHandlerInt sessionHandler) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

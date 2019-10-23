e214862c33b7b6892e7dae8f8ec618c01c5550b3
Alexander Eberle <2080955+alexander171294@users.noreply.github.com>
2019-10-23 15:21:41 -0300

    Instantiate PokerRoom class as GameCtrlInt

--- backend/room/src/main/java/ar/com/tandilweb/room/RoomServer.java
+++ backend/room/src/main/java/ar/com/tandilweb/room/RoomServer.java
@@ -3,9 +3,13 @@ package ar.com.tandilweb.room;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+
+import ar.com.tandilweb.room_int.GameCtrlInt;
+import ar.com.tandilweb.room_poker.PokerRoom;
 
 @SpringBootApplication
 @Configuration
@@ -18,5 +22,12 @@ public class RoomServer  extends SpringBootServletInitializer
     public static void main( String[] args )
     {
     	SpringApplication.run(RoomServer.class, args);
+    }
+    
+    @Bean()
+    public GameCtrlInt gameCtrl() {
+    	// TODO: check configuration for construct the specific class for all games.
+    	// TODO: send the server configuration to a constructor.
+    	return new PokerRoom();
     }
 }

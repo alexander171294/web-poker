4116aee635334bc2a1d0bee94af4f1c5f45e09f0
Alexander Eberle <2080955+alexander171294@users.noreply.github.com>
2019-11-02 17:25:07 -0300

    following.

--- frontend/newClient/src/app/pages/lobby/components/vcard/vcard.component.ts
+++ frontend/newClient/src/app/pages/lobby/components/vcard/vcard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-vcard',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vcard.component.scss']
 })
 export class VcardComponent implements OnInit {
+
+  @Input() status: string = '';
+  @Input() spectateButton: boolean;
+  @Input() statusColor;
+  @Input() playedHours: number;
+  @Input() nick: string;
 
   constructor() { }
 

fa38f87aa99ceae705d40141b4fe7b3bf42f80e6
Alexander Eberle <2080955+alexander171294@users.noreply.github.com>
2019-11-02 16:56:41 -0300

    Lobby initial design.

--- frontend/newClient/src/app/pages/lobby/components/table/table.component.ts
+++ frontend/newClient/src/app/pages/lobby/components/table/table.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  public rooms: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    for(let i = 1; i<=10; i++) {
+      this.rooms.push({
+        name: 'Room name #'+i,
+        description: 'Room description #'+i,
+        actualPlayers: 5+i,
+        maxPlayers: 11+i,
+        minChips: 500*i
+      });
+    }
   }
 
 }

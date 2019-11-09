be01b397c7f34c080cfa75b2337217f5eb9d9e78
Alexander Eberle <2080955+alexander171294@users.noreply.github.com>
2019-11-09 12:14:58 -0300

    poker table variables.

--- frontend/newClient/src/app/pages/room/tables/poker/poker.component.ts
+++ frontend/newClient/src/app/pages/room/tables/poker/poker.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerSnapshot } from './PlayerSnapshot';
+import { Card } from '../../cards/dual-stack/Card';
 
 @Component({
   selector: 'app-table-poker',
@@ -11,10 +12,15 @@ export class PokerComponent implements OnInit {
   private players: PlayerSnapshot[];
   private static MAX_PLAYERS = 10;
 
+  private tableCards: Card[];
+  private pot: number;
+  private dealed: boolean;
+
   constructor() { }
 
   ngOnInit() {
     this.players = [];
+    this.tableCards = [];
     for(let i = 0; i<PokerComponent.MAX_PLAYERS; i++) {
       this.players.push(new PlayerSnapshot());
     }

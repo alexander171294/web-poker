e0c0dc8ccc701cb67bbc2b47c93f3ea967765449
Alexander Eberle <alexander.eberle@sdc.com.ar>
2020-02-06 21:26:58 -0300

    profile data.

--- frontend/newClient/src/app/pages/lobby/lobby.component.ts
+++ frontend/newClient/src/app/pages/lobby/lobby.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { UserProfile } from './../../services/UserProfile';
+import { UsersService } from './../../services/users.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { LobbyService } from 'src/app/services/lobby.service';
 import { RoomResponse } from 'src/app/services/roomsResponse';
+import { Security } from 'src/app/services/Security';
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
   styleUrls: ['./lobby.component.scss']
 })
-export class LobbyComponent implements OnInit {
+export class LobbyComponent implements OnInit, OnDestroy {
 
   public version = environment.version;
   private rooms: RoomResponse[];
   public loadingProfileData = true;
+  public userData: UserProfile = new UserProfile();
+  public intUserData: any;
 
-  constructor(private lobbySrv: LobbyService) { }
+  constructor(private lobbySrv: LobbyService, private userSrv: UsersService) { }
 
   ngOnInit() {
     this.updateRooms();
+    this.loadingProfileData = true;
+    this.getProfileData();
+    this.intUserData = setInterval(() => this.getProfileData(), 1500);
   }
 
   updateRooms(callback?) {
@@ -30,7 +38,15 @@ export class LobbyComponent implements OnInit {
   }
 
   getProfileData() {
-    this.loadingProfileData = true;
+    this.userSrv.getUser(Security.getJWTData().iss).subscribe(data => {
+      this.loadingProfileData = false;
+      this.userData = data;
+      console.log(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intUserData);
   }
 
 }

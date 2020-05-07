379ecd5da6543c41683bbe67c1a8e62af63d0a21
Alexander Eberle <alexander.eberle@sdc.com.ar>
2020-05-07 12:47:05 -0300

    fixing room dockerfile.

--- backend/room/build-poker.sh
+++ backend/room/build-poker.sh
@@ -3,4 +3,4 @@ mvn clean package
 docker build -t tandilserver.com/web-poker/room-poker .
 docker push tandilserver.com/web-poker/room-poker
 docker rm room-poker
-docker run --name room-poker --env ORCHESTRATOR_IP="192.168.1.189" -p 8081:8081 -it tandilserver.com/web-poker/room-poker:latest
+docker run --name room-poker --env ORCHESTRATOR_IP="192.168.1.189" -p 8081:8081 -it tandilserver.com/web-poker/room-poker

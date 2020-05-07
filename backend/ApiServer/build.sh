b581bcba118a394a4aff4c90efaf9e89d5000d5a
Alexander Eberle <alexander.eberle@sdc.com.ar>
2020-05-07 13:43:53 -0300

    docker secrets.

--- backend/ApiServer/build.sh
+++ backend/ApiServer/build.sh
@@ -3,4 +3,5 @@ mvn clean package
 docker build -t tandilserver.com/web-poker/api-server .
 docker push tandilserver.com/web-poker/api-server
 docker rm apiServer
+# echo -n "smtp.gmail.com"|docker secret create mail.smtpserver -
 docker run --name apiServer --env DB_URL="jdbc:mysql://192.168.1.189:3307/poker?autoReconnect=true&useSSL=false" -p 8083:8083 -it tandilserver.com/web-poker/api-server:latest

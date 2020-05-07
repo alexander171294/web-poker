888712c49d14bd9fe8917e90dfd15444dada420e
Alexander Eberle <alexander.eberle@sdc.com.ar>
2020-05-07 12:44:55 -0300

    fixing dockers

--- backend/ApiServer/build.sh
+++ backend/ApiServer/build.sh
@@ -1,4 +1,6 @@
 rm -rf target
 mvn clean package
-docker build -t wearelantian.com/web-poker/api-server .
-docker push wearlantian.com/web-poker/api-server
+docker build -t tandilserver.com/web-poker/api-server .
+docker push tandilserver.com/web-poker/api-server
+docker rm apiServer
+docker run --name apiServer --env DB_URL="jdbc:mysql://192.168.1.189:3307/poker?autoReconnect=true&useSSL=false" -p 8083:8083 -it tandilserver.com/web-poker/api-server:latest
--- backend/orchestrator/build.sh
+++ backend/orchestrator/build.sh
@@ -1,4 +1,6 @@
 rm -rf target
 mvn clean package
-docker build -t wearelantian.com/web-poker/orchestrator .
-docker push wearlantian.com/web-poker/orchestrator
+docker build -t tandilserver.com/web-poker/orchestrator .
+docker push tandilserver.com/web-poker/orchestrator
+docker rm orchestrator
+docker run --name orchestrator --env DB_URL="jdbc:mysql://192.168.1.189:3307/poker?autoReconnect=true&useSSL=false" -p 8082:8082 -it tandilserver.com/web-poker/orchestrator:latest

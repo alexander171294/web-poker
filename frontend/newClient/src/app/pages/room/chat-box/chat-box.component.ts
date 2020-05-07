b5219d32bf922b3ec624fb80d6c5f6b40e809785
Alexander Eberle <alexander.eberle@sdc.com.ar>
2020-05-07 19:53:55 -0300

    unsuscribe chat.

--- frontend/newClient/src/app/pages/room/chat-box/chat-box.component.ts
+++ frontend/newClient/src/app/pages/room/chat-box/chat-box.component.ts
@@ -1,26 +1,28 @@
 import { ChatMessageData } from './ChatMessageData';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RoomService } from 'src/app/services/network/room.service';
 import { RxEType } from 'src/app/services/network/ReactionEvents';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.scss']
 })
-export class ChatBoxComponent implements OnInit {
+export class ChatBoxComponent implements OnInit, OnDestroy {
 
   opened: boolean;
   public chatMessages: ChatMessageData[] = [];
   public message: string;
 
   public playersInPositions: string[] = [];
+  public rxESubscription: Subscription;
 
   constructor(private room: RoomService) { }
 
   ngOnInit() {
     this.opened = true;
-    this.room.reactionEvent.subscribe(evt => {
+    this.rxESubscription = this.room.reactionEvent.subscribe(evt => {
       if (evt.type === RxEType.ANNOUNCEMENT) {
         this.playersInPositions[evt.data.position] = evt.data.user;
         this.chatMessages.push(new ChatMessageData(true, '@' + evt.data.user + ' is now in the room.'));
@@ -119,6 +121,10 @@ export class ChatBoxComponent implements OnInit {
   sendMessage() {
     this.room.chatMessage(this.message);
     this.message = '';
+  }
+
+  ngOnDestroy(): void {
+    this.rxESubscription.unsubscribe();
   }
 
 }

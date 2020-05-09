8607185aa09565e16e6c033de88dc44bc424e251
Alexander Eberle <alexander.eberle@sdc.com.ar>
2020-05-09 12:49:32 -0300

    cors and endpoint.

--- frontend/newClient/src/environments/environment.prod.ts
+++ frontend/newClient/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: '0.0.3-ALPHA',
-  apiServer: 'http://api-poker.tandilserver.com/',
+  apiServer: 'http://web-poker.tandilserver.com:8083/',
   refreshProfileTime: 2500,
   stompDebug: false
 };
